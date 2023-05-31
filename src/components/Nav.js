b7962ef754654a992722e4778cc071de448c047a
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2023-05-31 13:56:07 -0500

    setting up file structure

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>About Me</li>
+                    <li>Portfolio</li>
+                    <li>Resume</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Nav;
