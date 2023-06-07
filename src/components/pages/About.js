01be0c5f47bb1f61a9cf10626ca67dff09735646
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2023-06-06 22:37:04 -0500

    wave

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Me from '../site-images/brenda.png'
+import Wave from '../site-images/wave.png'
 
 import '../pages/About.css'
 
@@ -8,17 +9,22 @@ class About extends Component {
         return (
             <div className='about'
                 id='about'>
-                <div className='about-img-container'>
-                    <img src={Me} />
+                <div className='about-container'>
+                    <div className='about-img-container'>
+                        <img src={Me} />
+                    </div>
+                    <div className='about-content'>
+                        <h1>Hi, I'm Brenda!</h1>
+                        <p> I'm a full stack web developer with three years of experience in secondary science education personalizing the
+                            learning experience for students. Now looking to create esthetically pleasing and tailored experiences for users
+                            on the web. Earned a certificate in full stack development from the University of Texas in Austin, with newly
+                            developed skills in JavaScript, CSS, React.js, and responsive web design. Creative thinker and problem solver,
+                            ardent about web development. Ready for a new challenge and opportunity for growth personalizing user
+                            experience on the web.</p>
+                    </div>
                 </div>
-                <div className='about-content'>
-                    <h1>Hi, I'm Brenda!</h1>
-                    <p> I'm a full stack web developer with three years of experience in secondary science education personalizing the
-                        learning experience for students. Now looking to create esthetically pleasing and tailored experiences for users
-                        on the web. Earned a certificate in full stack development from the University of Texas in Austin, with newly
-                        developed skills in JavaScript, CSS, React.js, and responsive web design. Creative thinker and problem solver,
-                        ardent about web development. Ready for a new challenge and opportunity for growth personalizing user
-                        experience on the web.</p>
+                <div className='wave'>
+                    <img src={Wave} alt='wave'/>
                 </div>
             </div>
         )
